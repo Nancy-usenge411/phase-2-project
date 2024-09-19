b43030d3d37a3c9df95c085d240949a73ac7e9c5
Nancy Usenge <nancyusenge411@gmail.com>
2024-09-19 14:06:59 +0300

    commit changes

--- phase-2-project/src/node.js
+++ phase-2-project/src/node.js
@@ -1,4 +1,4 @@
-const PORT = 5000 node app.js;
+const PORT = 5000 (`node app.js`);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('Server is running on port ${port}');
